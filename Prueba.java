0c74079cd51743b4d738a50909c0722a3ef165d7
Kevin <k.silvares24@gmail.com>
2023-09-29 13:14:13 +0200

    Comida

--- Prueba.java
+++ Prueba.java
@@ -3,8 +3,8 @@ public class Prueba {
 	public static void main(String args[]) {
 
 		System.out.println("Silvares Carbonell");
-		System.out.println("Tengo 22 años y nací en el 2000");
-	
+		System.out.println("Tengo 22 años y nací en el 2000");		
+		System.out.println("Mi comida favorita es la paella");
 	}
 
 }
