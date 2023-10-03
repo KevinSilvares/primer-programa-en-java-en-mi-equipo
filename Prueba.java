a3f714f56a5eda99b19ff000e6ef3633e5c84bfa
Kevin <k.silvares24@gmail.com>
2023-10-03 13:17:39 +0200

    Prueba

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,6 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Silvares Carbonell");
 		System.out.println("Tengo 22 años y nací en el 2000");		
 		System.out.println("Mi comida favorita es la paella");
 	}
